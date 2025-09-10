153c53ae12fd00b0a365008688f662ce9d144d8d
David Lima <david.lima@ifal.edu.br>
2025-09-10 15:49:32 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="mb-8 border-4 border-solid border-blue-500 p-6 pl-10">
       <h1 className="bg-black text-red-500">Home page</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
