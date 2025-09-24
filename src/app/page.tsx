0a2b4189be28de59b22ddf77742f47e5823db27b
David Lima <david.lima@ifal.edu.br>
2025-09-24 14:29:20 -0300

    feat: add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,11 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { MenuIcon } from "lucide-react";
+import { MenuIcon, SearchIcon } from "lucide-react";
 
 import Header from "./_components/header";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -23,8 +25,22 @@ export default function Home() {
         <p className="text-sm">Quarta-feira, 24 de setembro de 2025</p>
       </div>
       {/*Buscar*/}
+      <div className="flex flex-row items-center gap-4 px-5">
+        <Input placeholder="Buscar serviços" />
+        <Button size={"icon"}>
+          <SearchIcon />
+        </Button>
+      </div>
       {/*Busca rápida*/}
       {/*Banner*/}
+      <div className="relative mt-6 h-[150px] w-full p-5">
+        <Image
+          src="/banner-01.png"
+          alt="Banner"
+          fill
+          className="rounded-xl object-contain"
+        />
+      </div>
       {/*Agendamentos*/}
       {/*Recomendados*/}
       {/*Populares*/}
